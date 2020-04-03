e54aa65093277199a764e5833af696f49fdeecef
PANFACTORY <yutatanabe510@gmail.com>
2020-04-04 00:04:58 +0900

    Add graphicsjs

--- template/modules.js
+++ template/modules.js
@@ -1,16 +1,12 @@
-const containerxiita = document.getElementById('containerxiita');
-const canvasxiita = document.getElementById('canvasxiita');
-const ctxxiita = canvasxiita.getContext('2d');
+const canvasxiita = acgraph.create('canvasxiita');
 const inputnxi = document.getElementById('inputnxi');
 const inputnita = document.getElementById('inputnita');
-const containerxy = document.getElementById('containerxy');
-const canvasxy = document.getElementById('canvasxy');
-const ctxxy = canvasxy.getContext('2d');
+const canvasxy = acgraph.create('canvasxy');
 
 
 inputnxi.addEventListener('change', updatenxiita);
 inputnita.addEventListener('change', updatenxiita);
-window.addEventListener('resize', resizewindow, false);
+
 canvasxy.addEventListener('click', drawxy);
 
 
@@ -22,30 +18,28 @@ function updatenxiita(){
     if(Number(inputnita.value) == 0){
         inputnita.value = 1;
     }
-    ctxxiita.clearRect(0, 0, canvasxiita.width, canvasxiita.height);
-    
+    canvasxiita.removeChildren();
+
     //----------Draw mesh----------
-    ctxxiita.strokeStyle = "aqua";
-    ctxxiita.lineWidth = 1;
-    ctxxiita.beginPath();
-    var delta = 0.8*Math.min(canvasxiita.width/Number(inputnxi.value), canvasxiita.height/Number(inputnita.value));
-    var offsetxi = 0.5*(canvasxiita.width - delta*Number(inputnxi.value));
-    var offsetita = 0.5*(canvasxiita.height - delta*Number(inputnita.value));
+    var delta = 0.8*Math.min(canvasxiita.width()/Number(inputnxi.value), canvasxiita.height()/Number(inputnita.value));
+    var offsetxi = 0.5*(canvasxiita.width() - delta*Number(inputnxi.value));
+    var offsetita = 0.5*(canvasxiita.height() - delta*Number(inputnita.value));
+    
     for(var v = 0; v < Number(inputnxi.value) + 1; v++){
-        ctxxiita.moveTo(v*delta + offsetxi, offsetita);
-        ctxxiita.lineTo(v*delta + offsetxi, Number(inputnita.value)*delta + offsetita);
+        canvasxiita.path()
+            .moveTo(v*delta + offsetxi, offsetita)
+            .lineTo(v*delta + offsetxi, Number(inputnita.value)*delta + offsetita)
+            .stroke('aqua');
     }
     for(var v = 0; v < Number(inputnita.value) + 1; v++){
-        ctxxiita.moveTo(offsetxi, v*delta + offsetita);
-        ctxxiita.lineTo(Number(inputnxi.value)*delta + offsetxi, v*delta + offsetita);
+        canvasxiita.path()
+            .moveTo(offsetxi, v*delta + offsetita)
+            .lineTo(Number(inputnxi.value)*delta + offsetxi, v*delta + offsetita)
+            .stroke('aqua');
     }
 
-    ctxxiita.stroke();
-
-    ctxxiita.beginPath();
-
     //----------Draw coordinate----------
-    ctxxiita.strokeStyle = "white";
+/*    ctxxiita.strokeStyle = "white";
     ctxxiita.moveTo(10, canvasxiita.height - 10);
     ctxxiita.lineTo(50, canvasxiita.height - 10);
     ctxxiita.moveTo(10, canvasxiita.height - 10);
@@ -66,6 +60,7 @@ function updatenxiita(){
     ctxxiita.font = "15px 'Times New Roman'";
     ctxxiita.fillText("ξ", 55, canvasxiita.height - 25);
     ctxxiita.fillText("η", 5, canvasxiita.height - 70);
+*/
 }
 
 
@@ -76,15 +71,7 @@ function drawxy(e){
 }
 
 
-function resizewindow(){
-    canvasxiita.width = containerxiita.clientWidth;
-    canvasxiita.height = containerxiita.clientHeight;
-    
-    canvasxy.width = containerxy.clientWidth;
-    canvasxy.height = containerxy.clientHeight;
-    
-    updatenxiita();
-}
+updatenxiita();
 
  
-resizewindow();
+//resizewindow();
