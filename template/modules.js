dc121aa70b7f7fe66ec5a8e42c6fd0d4fe42ba7c
PANFACTORY <yutatanabe510@gmail.com>
2020-04-04 15:02:57 +0900

    Add paint

--- template/modules.js
+++ template/modules.js
@@ -1,7 +1,9 @@
 const inputnxi = document.getElementById('inputnxi');
 const inputnita = document.getElementById('inputnita');
+const buttonline = document.getElementById('buttonline');
 
 
+//********************ξη座標系ステージ********************
 const stagexiita = new createjs.Stage('canvasxiita');       //  ξη座標系ステージ
 const xiitacoordinate = new createjs.Container();           //  ξη座標系座標軸コンテナ
 stagexiita.addChild(xiitacoordinate);
@@ -28,7 +30,7 @@ function drawxiitacoordinate(){
     var textita = new createjs.Text("η", "italic 15px Times New Roman", "white");
     textita.x = 5;
     textita.y = stagexiita.canvas.height - 75;
-    xiitacoordinate.addChild(textita);   
+    xiitacoordinate.addChild(textita);
     
     stagexiita.update();
 }
@@ -36,102 +38,128 @@ function drawxiitacoordinate(){
 
 function drawxiitamesh(){
     //----------Initialize container----------
-    xiitamesh.removeAllChildren();
-
-    //----------Draw mesh----------
-}
-
-
-
-
-
-
-
-//const canvasxy = acgraph.create('canvasxy');
-//const layerxycordinate = canvasxy.layer();                  //  canvasxyの座標軸描画用レイヤー
-//const layerxymesh = canvasxy.layer();                       //  canvasxyのメッシュ描画用レイヤー
-const buttonline = document.getElementById('buttonline');
-
-
-inputnxi.addEventListener('change', updatecanvasxiita);
-inputnita.addEventListener('change', updatecanvasxiita);
-window.addEventListener('resize', resizewindow, false);
-/*layerxymesh.listen('click', function(){
-    console.log(canvasxy.data());
-});*/
-
-
-//layerxymesh.rect(10, 10, 100, 100);
-
-
-function updatecanvasxiita(){
-/*    //----------Initialize canvas----------
     if(Number(inputnxi.value) == 0){
         inputnxi.value = 1;
     }
     if(Number(inputnita.value) == 0){
         inputnita.value = 1;
     }
-    canvasxiita.removeChildren();
+    xiitamesh.removeAllChildren();
 
     //----------Draw mesh----------
-    var delta = 0.8*Math.min(canvasxiita.width()/Number(inputnxi.value), canvasxiita.height()/Number(inputnita.value));
-    var offsetxi = 0.5*(canvasxiita.width() - delta*Number(inputnxi.value));
-    var offsetita = 0.5*(canvasxiita.height() - delta*Number(inputnita.value));
+    var delta = 0.8*Math.min(stagexiita.canvas.width/Number(inputnxi.value), stagexiita.canvas.height/Number(inputnita.value));
+    var offsetxi = 0.5*(stagexiita.canvas.width - delta*Number(inputnxi.value));
+    var offsetita = 0.5*(stagexiita.canvas.height - delta*Number(inputnita.value));
     
+    var line = new createjs.Graphics();
     for(var v = 0; v < Number(inputnxi.value) + 1; v++){
-        canvasxiita.path()
-            .moveTo(v*delta + offsetxi, offsetita)
-            .lineTo(v*delta + offsetxi, Number(inputnita.value)*delta + offsetita)
-            .stroke('aqua');
+        line.s('aqua').mt(v*delta + offsetxi, offsetita).lt(v*delta + offsetxi, Number(inputnita.value)*delta + offsetita).es();
     }
     for(var v = 0; v < Number(inputnita.value) + 1; v++){
-        canvasxiita.path()
-            .moveTo(offsetxi, v*delta + offsetita)
-            .lineTo(Number(inputnxi.value)*delta + offsetxi, v*delta + offsetita)
-            .stroke('aqua');
+        line.s('aqua').mt(offsetxi, v*delta + offsetita).lt(Number(inputnxi.value)*delta + offsetxi, v*delta + offsetita).es();
     }
-
-    //----------Draw coordinate----------
-    canvasxiita.path()
-        .moveTo(10, canvasxiita.height() - 15)
-        .lineTo(50, canvasxiita.height() - 15)
-        .stroke('red');
-    canvasxiita.path()
-        .moveTo(10, canvasxiita.height() - 15)
-        .lineTo(10, canvasxiita.height() - 55)
-        .stroke('yellow');
-    cordinatexiitatextstyle = {fontFamily: 'Times New Roman', fontStyle: 'italic', fontSize: '15px', color: 'white'};
-    canvasxiita.text(55, canvasxiita.height() - 25, "ξ", cordinatexiitatextstyle);
-    canvasxiita.text(5, canvasxiita.height() - 75, "η", cordinatexiitatextstyle);
+    var shape = new createjs.Shape(line);
+    xiitamesh.addChild(shape);
 
     //----------Draw point marker----------
-    pointmarkertextstyle = {fontFamily: 'Times New Roman', fontSize: '15px', color: 'lightsalmon'};
-    canvasxiita.text(offsetxi - 10, Number(inputnita.value)*delta + offsetita, '0', pointmarkertextstyle);
-    canvasxiita.text(Number(inputnxi.value)*delta + offsetxi + 5, Number(inputnita.value)*delta + offsetita, '1', pointmarkertextstyle);
-    canvasxiita.text(Number(inputnxi.value)*delta + offsetxi + 5, offsetita - 10, '2', pointmarkertextstyle);
-    canvasxiita.text(offsetxi - 10, offsetita - 10, '3', pointmarkertextstyle);
-*/
+    var textmarker0 = new createjs.Text("0", "15px Times New Roman", "lightsalmon");
+    textmarker0.x = offsetxi - 10;
+    textmarker0.y = Number(inputnita.value)*delta + offsetita;
+    xiitamesh.addChild(textmarker0);
+
+    var textmarker1 = new createjs.Text("1", "15px Times New Roman", "lightsalmon");
+    textmarker1.x = Number(inputnxi.value)*delta + offsetxi + 5;
+    textmarker1.y = Number(inputnita.value)*delta + offsetita;
+    xiitamesh.addChild(textmarker1);
+
+    var textmarker2 = new createjs.Text("2", "15px Times New Roman", "lightsalmon");
+    textmarker2.x = Number(inputnxi.value)*delta + offsetxi + 5;
+    textmarker2.y = offsetita - 10;
+    xiitamesh.addChild(textmarker2);
+
+    var textmarker3 = new createjs.Text("3", "15px Times New Roman", "lightsalmon");
+    textmarker3.x = offsetxi - 10;
+    textmarker3.y = offsetita - 10;
+    xiitamesh.addChild(textmarker3);
+
+    stagexiita.update();
 }
 
 
-/*function updatecanvasxy(){
-    //----------Initialize canvasxy----------
-    layerxycordinate.removeChildren();
+//********************xy座標系ステージ********************
+const stagexy = new createjs.Stage('canvasxy');             //  xy座標系ステージ
+const xycoordinate = new createjs.Container();              //  xy座標系座標軸コンテナ
+stagexy.addChild(xycoordinate);
+const xymesh = new createjs.Container();                    //  xy座標系メッシュコンテナ
+stagexy.addChild(xymesh);
+const xytmp = new createjs.Container();                     //  xy座標系下書きコンテナ
+stagexy.addChild(xytmp);
+var xyx0 = 0;
+var xyy0 = 0;
+
+
+
+function drawxycoordinate(){
+    //----------Initialize container----------
+    xycoordinate.removeAllChildren();
 
     //----------Draw coordinate----------
-    layerxycordinate.path()
-        .moveTo(10, canvasxy.height() - 15)
-        .lineTo(50, canvasxy.height() - 15)
-        .stroke('red');
-    layerxycordinate.path()
-        .moveTo(10, canvasxy.height() - 15)
-        .lineTo(10, canvasxy.height() - 55)
-        .stroke('yellow');
-    cordinatexytextstyle = {fontFamily: 'Times New Roman', fontStyle: 'italic', fontSize: '15px', color: 'white'};
-    layerxycordinate.text(55, canvasxy.height() - 25, "x", cordinatexytextstyle);
-    layerxycordinate.text(5, canvasxy.height() - 75, "y", cordinatexytextstyle);
-}*/
+    var line = new createjs.Graphics();
+    line.s('red').mt(10, stagexy.canvas.height - 15).lt(50, stagexy.canvas.height - 15).es();
+    line.s('yellow').mt(10, stagexy.canvas.height - 15).lt(10, stagexy.canvas.height - 55).es();
+    var shape = new createjs.Shape(line);
+    xycoordinate.addChild(shape);
+
+    var textx = new createjs.Text("x", "italic 15px Times New Roman", "white");
+    textx.x = 55;
+    textx.y = stagexy.canvas.height - 25;
+    xycoordinate.addChild(textx);
+
+    var texty = new createjs.Text("y", "italic 15px Times New Roman", "white");
+    texty.x = 5;
+    texty.y = stagexy.canvas.height - 75;
+    xycoordinate.addChild(texty);
+    
+    stagexy.update();
+}
+
+
+function xymousedown(event){
+    xyx0 = event.stageX;
+    xyy0 = event.stageY;
+    stagexy.addEventListener("stagemousemove", xymousemove);
+    stagexy.addEventListener("stagemouseup", xymouseup);
+}
+
+
+function xymousemove(event){
+    xytmp.removeAllChildren();
+    var line = new createjs.Graphics();
+    line.s('lightsalmon').mt(xyx0, xyy0).lt(event.stageX, event.stageY).es();
+    var shape = new createjs.Shape(line);
+    xytmp.addChild(shape);
+    stagexy.update();
+}
+
+
+function xymouseup(event){
+    xytmp.removeAllChildren();
+    var line = new createjs.Graphics();
+    line.s('aqua').mt(xyx0, xyy0).lt(event.stageX, event.stageY).es();
+    var shape = new createjs.Shape(line);
+    xymesh.addChild(shape);
+    stagexy.update();
+
+    stagexy.removeEventListener("stagemousemove", xymousemove);
+    stagexy.removeEventListener("stagemouseup", xymouseup);
+}
+
+
+//********************イベント********************
+inputnxi.addEventListener('change', drawxiitamesh);
+inputnita.addEventListener('change', drawxiitamesh);
+window.addEventListener('resize', resizewindow, false);
+stagexy.addEventListener('stagemousedown', xymousedown);
 
 
 function resizewindow(){
@@ -142,10 +170,11 @@ function resizewindow(){
     drawxiitacoordinate();
     drawxiitamesh();
     
+    stagexy.canvas.width = document.getElementById('canvasxy').parentNode.clientWidth;
+    stagexy.canvas.height = document.getElementById('canvasxy').parentNode.clientHeight;
+    stagexy.update();
     
-   
-    //canvasxy.resize(document.getElementById('canvasxy').parentNode.clientWidth, document.getElementById('canvasxy').parentNode.clientHeight);
-    //updatecanvasxy();
+    drawxycoordinate();
 }
 
 
